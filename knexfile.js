bf4857aa29557b3e7b2e36176f73d3c89f136b44
Steve Witman <stevewitman@gmail.com>
2016-09-21 23:36:25 -0600

    add modules and generate knexfile

---
+++ knexfile.js
@@ -0,0 +1,26 @@
+module.exports =
+{
+  development:
+  {
+    client: "sqlite3",
+    connection: { filename: "./movie.sqlite"  },
+    migrations: { tableName: 'knex_migrations'},
+    seeds:      { directory: './seeds'        },
+    debug: false
+  },
+
+  production:
+  {
+    client: "pg",
+    connection:
+    {
+      host    : 'localhost',
+      user    : 'postgres',
+      database: 'movie',
+      password: 'sprite',
+    },
+    migrations: { tableName: 'knex_migrations' },
+    seeds:      { directory: './seeds'         },
+    debug: false
+  }
+};
